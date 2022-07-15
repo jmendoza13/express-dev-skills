a9f8f17c3e465dd65b88390bd97031c026b7a3f0
jmendoza13 <justinmendoza13@gmail.com>
2022-07-15 13:31:44 -0700

    final submission for lab

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(methodOverride('_method'));
 
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   console.log('Hello!');
   res.locals.time = new Date().toLocaleDateString();
   next();
@@ -32,13 +32,13 @@ app.use('/', indexRouter);
 app.use('/skills', skillsRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
+app.use(function (err, req, res, next) {
+  /// set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
