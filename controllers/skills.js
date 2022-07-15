171dc66d4538823919b5cebfedf5ded759e6829c
jmendoza13 <justinmendoza13@gmail.com>
2022-07-15 13:33:30 -0700

    fixed code structure to be clean

--- controllers/skills.js
+++ controllers/skills.js
@@ -1,39 +1,38 @@
 const Skill = require('../models/skill');
 
 module.exports = {
-      index,
-      show,
-      new: newSkill,
-      create,
-      delete: deleteSkill
-    };
+    index,
+    show,
+    new: newSkill,
+    create,
+    delete: deleteSkill
+};
 
-    function deleteSkill(req, res) {
-        Skill.deleteOne(req.params.id);
-        res.redirect('/skills');
-    }
+function deleteSkill(req, res) {
+    Skill.deleteOne(req.params.id);
+    res.redirect('/skills');
+}
 
-    function create(req, res) {
-        console.log(req.body);
-        Skill.create(req.body);
-        res.redirect('/skills');
-  }
+function create(req, res) {
+    console.log(req.body);
+    Skill.create(req.body);
+    res.redirect('/skills');
+}
 
-    
-  function newSkill(req, res) {
-      res.render('skills/new');
-  }
-    
-  function show(req, res) {
-      const skill = Skill.getOne(req.params.id);
-      res.render('skills/show', {skill})
-  };
 
-    function index(req, res) {
-        const skills = Skill.getAll();
-        res.render('skills/index', { skills });
-    };
-    
+function newSkill(req, res) {
+    res.render('skills/new');
+}
 
-      
-    
+function show(req, res) {
+    const skill = Skill.getOne(req.params.id);
+    res.render('skills/show', { skill })
+};
+
+function index(req, res) {
+    const skills = Skill.getAll();
+    res.render('skills/index', { skills });
+};
+
+
+
--- models/skill.js
+++ models/skill.js
@@ -1,37 +1,36 @@
 const skills = [
-    {id: 125223, skill: 'Become a Barista', accomplished: true},
-    {id: 127904, skill: 'Become a Coffee Master', accomplished: true},
-    {id: 139608, skill: 'Latte Art Connoisseur', accomplished: false}
-  ];
-  
-  module.exports = {
-    getAll,
-    getOne,
-    create,
-    deleteOne
-  };
+  { id: 125223, skill: 'Become a Barista', accomplished: true },
+  { id: 127904, skill: 'Become a Coffee Master', accomplished: true },
+  { id: 139608, skill: 'Latte Art Connoisseur', accomplished: false }
+];
 
-  function deleteOne(id) {
-    // All properties attached to req.params are strings!
-    id = parseInt(id);
-    // Find the index based on the id of the skill object
-    const idx = skills.findIndex( skill => skill.id === id);
-    skills.splice(idx, 1);
-  }
+module.exports = {
+  getAll,
+  getOne,
+  create,
+  deleteOne
+};
+
+function deleteOne(id) {
+  // All properties attached to req.params are strings!
+  id = parseInt(id);
+  // Find the index based on the id of the skill object
+  const idx = skills.findIndex(skill => skill.id === id);
+  skills.splice(idx, 1);
+}
 
 
-  function create(skill) {
-    skill.id = Date.now() %1000000;
-    skill.done = false;
-    skills.push(skill);
-  }
+function create(skill) {
+  skill.id = Date.now() % 1000000;
+  skill.done = false;
+  skills.push(skill);
+}
 
-  function getOne(id) {
-    id = parseInt(id);
-    return skills.find(skill => skill.id === id);
-  }
-  
-  function getAll() {
-    return skills;
-  }
-  
+function getOne(id) {
+  id = parseInt(id);
+  return skills.find(skill => skill.id === id);
+}
+
+function getAll() {
+  return skills;
+}
